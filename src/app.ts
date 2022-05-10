bd2d5d93f8c9263eb7f0d524b7b5dcc05a33e492
Christian Nuss <christian.nuss@gmail.com>
2022-05-10 07:54:01 -0700

    remove terraformErrorHandler

--- src/app.ts
+++ src/app.ts
@@ -2,27 +2,13 @@ import {
   corsHandler,
   CorsOptions,
   createApp,
-  errorHandler,
   registerDocs,
   registerVersion,
 } from '@scaffoldly/serverless-util';
-import express, { NextFunction, Request, Response } from 'express';
+import express from 'express';
 import { readFileSync } from 'fs';
 import packageJson from '../package.json';
-import { TerraformError } from './interfaces/errors';
 import { RegisterRoutes } from './routes';
-
-export function terraformErrorHandler(version: string) {
-  return (err: Error | any, req: Request, res: Response, next: NextFunction): Response | void => {
-    if (err instanceof TerraformError) {
-      err.respond(res);
-      next();
-      return;
-    }
-
-    return errorHandler(version)(err, req, res, next);
-  };
-}
 
 import swaggerJson from './swagger.json';
 
@@ -33,8 +19,6 @@ const corsOptions: CorsOptions = {};
 app.use(corsHandler(corsOptions));
 
 RegisterRoutes(app);
-
-app.use(terraformErrorHandler(packageJson.version));
 
 registerDocs(app, swaggerJson);
 registerVersion(app, packageJson.version);
